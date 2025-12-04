b318899b15d2543cb17a7f55e5471de2c57b6dc8
Petr Melicharik <petr.melicharik@seznam.cz>
2025-12-04 21:20:46 +0100

    repair

--- pages/02_forma.py
+++ pages/02_forma.py
@@ -20,5 +20,5 @@ shortlist2 = pd.merge(shortlist, ratings, on="id")
 database = pd.merge(shortlist2, stats, on="id")
 
 # zobrazení top 60 ratingů 
-form = database[["Jméno", "Příjmení", "team", "Pozice", "avg_rating", "Sofascore"]].sort_values("avg_rating", ascending=False) st.dataframe(form.head(60))
+form = database[["Jméno", "Příjmení", "team", "Pozice", "avg_rating", "Sofascore"]].sort_values("avg_rating", ascending=False)
 st.dataframe(form.head(60))
