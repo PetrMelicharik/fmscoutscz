f9b8414b3fe6d8f65158621084fab6674e6de4e1
Petr Melicharik <petr.melicharik@seznam.cz>
2025-12-06 11:06:50 +0100

    better stats

--- pages/04_statistiky.py
+++ pages/04_statistiky.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="FM Scouts CZ - forma", page_icon="⚽")
 # záhlaví stránky
 st.image("logo.jpg", width=100)
 st.title("FM Scouts cz")
-st.subheader("Statistiky:")
+st.subheader("Nejlepší střelci:")
 
 # načtení excel souborů
 players = pd.read_excel("players.xlsx")
@@ -20,6 +20,16 @@ shortlist = pd.merge(players, profiles, on="id")
 shortlist2 = pd.merge(shortlist, ratings, on = "id")
 database = pd.merge(shortlist2, stats, on="id")
 
-# zobrazení top 50 ratingů
-stats = database[["Jméno", "Příjmení", "team", "Pozice", "appearances", "minutesPlayed", "goals", "assists", "goalsAssistsSum", "Sofascore"]]
-st.dataframe(stats)
+# zobrazení top 20 střelců
+stats = database[["Jméno", "Příjmení", "team", "Pozice", "goals", "Sofascore"]].sort_values("goals", ascending=False)
+st.dataframe(stats.head(20))
+
+# zobrazení top 20 asistentů
+st.subheader("Nejlepší nahrávači:")
+stats = database[["Jméno", "Příjmení", "team", "Pozice", "assists", "Sofascore"]].sort_values("assists", ascending=False)
+st.dataframe(stats.head(20))
+
+# zobrazení top 20 v bodech
+st.subheader("Kanadské bodování:")
+stats = database[["Jméno", "Příjmení", "team", "Pozice", "goalsAssistsSum", "Sofascore"]].sort_values("goalsAssistsSum", ascending=False)
+st.dataframe(stats.head(20))
