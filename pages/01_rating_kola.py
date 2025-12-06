7400ac55eb9f097509f73a1395f88c87589b1385
Petr Melicharik <petr.melicharik@seznam.cz>
2025-12-06 08:41:40 +0100

    filtered rating with date

--- pages/01_rating_kola.py
+++ pages/01_rating_kola.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime, timedelta
 
 # nastavení stránky - kosmetická úprava
 st.set_page_config(page_title="FM Scouts CZ - ratings", page_icon="⚽")
@@ -20,6 +21,13 @@ shortlist = pd.merge(players, profiles, on="id")
 shortlist2 = pd.merge(shortlist, ratings, on = "id")
 database = pd.merge(shortlist2, stats, on="id")
 
-# zobrazení top 50 ratingů
-potw = database[["Jméno", "Příjmení", "team", "Pozice", "Rating_1", "Sofascore"]].sort_values("Rating_1", ascending=False)
-st.dataframe(potw.head(60))
+# převod date 1 na datum
+database["Date_1"]=pd.to_datetime(database["Date_1"], errors="coerce").dt.date
+
+today = datetime.today().date()
+
+database_filtered = database[(today - database["Date_1"]) <= timedelta(days=7)]
+
+# zobrazení top 30 ratingů
+potw = database_filtered[["Jméno", "Příjmení", "team", "Pozice", "Rating_1", "Sofascore"]].sort_values("Rating_1", ascending=False)
+st.dataframe(potw.head(30))
